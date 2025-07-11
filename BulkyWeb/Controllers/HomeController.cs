7622e80b5fb33f08080653eb300b33e1e1d7ed3c
jeffmikhail <jeffmikhail.dev@gmail.com>
2025-07-11 14:33:35 +0800

    improve UI

--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace BulkyWeb.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
