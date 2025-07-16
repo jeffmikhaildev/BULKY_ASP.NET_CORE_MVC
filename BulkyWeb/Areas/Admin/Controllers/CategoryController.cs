3b8f1ca34db9e28da972f49f064537c61a9667b4
jeffmikhail <jeffmikhail.dev@gmail.com>
2025-07-16 22:35:27 +0800

    added Admin and Customer areas

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -4,8 +4,9 @@ using BulkyBook.DataAccess.Repository.Irepository;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyBookWeb.Controllers
+namespace BulkyBookWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
